cfdd60759306021c061a8579d45cef3ea2778cec
Alvaro Orellana <tiveor@gmail.com>
2016-01-16 10:01:29 -0400

    Analytics Updated

--- ParseAnalytics2Test/app/src/main/java/com/possiblelabs/parseanalytics2test/MainActivity.java
+++ ParseAnalytics2Test/app/src/main/java/com/possiblelabs/parseanalytics2test/MainActivity.java
@@ -3,8 +3,6 @@ package com.possiblelabs.parseanalytics2test;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -28,7 +26,8 @@ public class MainActivity extends BaseTrackeableActivity {
 
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 startActivity(intent);
-                ParseAnalytics.trackEventInBackground("CLICK_IN_BUTTON");            }
+                ParseAnalytics.trackEventInBackground("CLICK_IN_BUTTON");
+            }
         });
     }
 
