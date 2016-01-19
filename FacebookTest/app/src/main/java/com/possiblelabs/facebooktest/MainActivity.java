b17210a8dc7f06809af0e21dbcec7230c0c2fadf
Alvaro Orellana <tiveor@gmail.com>
2016-01-19 19:45:27 -0400

    FacebookTest Update

--- FacebookTest/app/src/main/java/com/possiblelabs/facebooktest/MainActivity.java
+++ FacebookTest/app/src/main/java/com/possiblelabs/facebooktest/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.facebook.AccessToken;
@@ -14,11 +15,18 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphRequestBatch;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
 import com.facebook.Profile;
 import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.facebook.login.widget.ProfilePictureView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
@@ -30,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
     private TextView userNameView;
     private ProfilePictureView profilePictureView;
     private Profile currentProfile;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,15 +52,21 @@ public class MainActivity extends AppCompatActivity {
 
         LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions(Arrays.asList("public_profile", "user_friends"));
+//        loginButton.setPublishPermissions(Arrays.asList("publish_actions"));
+
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 // App code
                 Log.d("FB", loginResult.getAccessToken().getToken());
-                Profile p = Profile.getCurrentProfile();
-                Log.d("FB", p.getName());
+                currentProfile = Profile.getCurrentProfile();
 
+                if (currentProfile != null && currentProfile.getName() != null) {
+                    Log.d("FB", currentProfile.getName());
+                } else {
+                    Log.d("FB", "null profile");
+                }
             }
 
             @Override
@@ -63,7 +76,6 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onError(FacebookException exception) {
-
             }
         });
 
@@ -72,7 +84,6 @@ public class MainActivity extends AppCompatActivity {
             protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
                 MainActivity.this.currentProfile = currentProfile;
                 setProfile(currentProfile);
-
             }
         };
 
@@ -104,6 +115,90 @@ public class MainActivity extends AppCompatActivity {
             profilePictureView.setProfileId(profile.getId());
             userNameView.setText(profile.getName());
         }
+
+
+    }
+
+    public void getFriends(View view) {
+
+        Bundle parameters = new Bundle();
+        parameters.putInt("limit", 10);
+
+
+        AccessToken access_token = AccessToken.getCurrentAccessToken();
+
+        GraphRequestBatch batch = new GraphRequestBatch(
+                GraphRequest.newMeRequest(
+                        access_token,
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(
+                                    JSONObject jsonObject,
+                                    GraphResponse response) {
+                                // Application code for user
+                            }
+                        }),
+                GraphRequest.newMyFriendsRequest(
+                        access_token,
+                        new GraphRequest.GraphJSONArrayCallback() {
+                            @Override
+                            public void onCompleted(
+                                    JSONArray jsonArray,
+                                    GraphResponse response) {
+                                Log.d("FB", "onCompleted");
+                        JSONObject baseJson = response.getJSONObject();
+
+                        try {
+                            Log.d("FB", baseJson.toString());
+                            JSONArray arrayJson = baseJson.getJSONArray("data");
+                            Log.d("FB", arrayJson.length() + "<<");
+                            for (int i = 0; i < arrayJson.length(); i++) {
+                                JSONObject friend = arrayJson.getJSONObject(i);
+                                Log.d("FB", friend.getString("name").toString());
+                            }
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                            }
+                        })
+        );
+        batch.addCallback(new GraphRequestBatch.Callback() {
+            @Override
+            public void onBatchCompleted(GraphRequestBatch graphRequests) {
+                // Application code for when the batch finishes
+            }
+        });
+        batch.executeAsync();
+
+//
+//        GraphRequest.newMyFriendsRequest(
+//                AccessToken.getCurrentAccessToken(),
+//                "v2.2/me/friends",
+//                parameters,
+//                HttpMethod.GET,
+//                new GraphRequest.Callback() {
+//                    public void onCompleted(GraphResponse response) {
+//                        Log.d("FB", "onCompleted");
+//                        JSONObject baseJson = response.getJSONObject();
+//
+//                        try {
+//                            Log.d("FB", baseJson.toString());
+//                            JSONArray arrayJson = baseJson.getJSONArray("data");
+//                            Log.d("FB", arrayJson.length() + "<<");
+//                            for (int i = 0; i < arrayJson.length(); i++) {
+//                                JSONObject friend = arrayJson.getJSONObject(i);
+//                                Log.d("FB", friend.getString("name").toString());
+//                            }
+//
+//                        } catch (Exception e) {
+//                            e.printStackTrace();
+//                        }
+//
+//
+//                    }
+//                }
+//        ).executeAsync();
     }
 
     @Override
